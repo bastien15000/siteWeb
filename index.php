5883e02719fa907ac441ba0e1147a78b3b80d1ed
bastien <bastientakis@gmail.com>
2020-06-30 22:31:47 +0200

    commit avec php et css

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="index.css">
 	<title>Ma page web</title>
 </head>
 <body>
 	<h1>Ma première page web upload</h1>
+	<?php
+		phpinfo();
+	?>
 </body>
 </html>
