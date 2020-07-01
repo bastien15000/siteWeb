09d6ce946d4f81f02967c167cfbdff8ef0d6bf6f
bastien <bastientakis@gmail.com>
2020-07-01 22:04:05 +0200

    commit test php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<title>Ma page web</title>
+</head>
+<body>
+	<h1>Ma première page web upload</h1>
+	<?php phpinfo(); ?>
+</body>
+</html>
